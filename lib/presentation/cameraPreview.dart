1a061523c4571e9d1acfa578d0c225896b38b146
Navdeep <navdeep@aeologic.com>
2022-12-16 19:07:39 +0530

    Filter added Overlay icon added.

--- lib/presentation/cameraPreview.dart
+++ lib/presentation/cameraPreview.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
+import 'package:camera_0verlay/presentation/image_prview_filter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../capture/video_capture_bloc.dart';
 import 'image_preview.dart';
+import 'package:image/image.dart' as imageLib;
+import 'package:path/path.dart' as path;
 
 class VideoCaptureForm extends StatefulWidget {
   const VideoCaptureForm({Key? key}) : super(key: key);
@@ -21,18 +26,23 @@ class _VideoCaptureFormState extends State<VideoCaptureForm> {
       ..add(ControllerInitializeEvent());
   }
 
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: BlocConsumer<VideoCaptureBloc, VideoCaptureState>(
-        listener: (context, state) {
-          if (state is VideoRecordFinishedState) {
-            final route = MaterialPageRoute(
+        listener: (context, state)async {
+          if (state is VideoRecordFinishedState){
+            File? imageFile = File(state.file.path);
+            String? fileName = path.basename(imageFile.path);
+            var image = imageLib.decodeImage(await imageFile.readAsBytes());
+            Map imagefile  = await Navigator.push(context,MaterialPageRoute(
               fullscreenDialog: true,
-              builder: (_) => ImagePreviewPage(filePath: state.file.path),
-            );
+              builder: (_) => ImagePreviewPage(filePath: fileName,image: image!),
+            ));
 
-            Navigator.push(context, route);
+            Navigator.push(context, MaterialPageRoute(builder: (_) => ImagePreviewFilter(imageFile: imagefile['image_filtered'],)));
           }
           if (state is ControllerInitializedState) {
             // setState(() {
@@ -165,28 +175,38 @@ class _VideoCaptureFormState extends State<VideoCaptureForm> {
   }
 }
 Widget _getOverlay() {
-  return ColorFiltered(
-    colorFilter: ColorFilter.mode(
-        Colors.black54,
-        BlendMode.srcOut
-    ),
+  return Padding(
+    padding: const EdgeInsets.only(bottom: 50),
     child: Stack(
+      alignment: Alignment.center,
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.transparent,
+        Positioned(child: Icon(Icons.person_outline,size: 150,color: Colors.grey.shade800,)),
+        ColorFiltered(
+          colorFilter: ColorFilter.mode(
+              Colors.black54,
+              BlendMode.srcOut
           ),
-          child: Align(
-            alignment: Alignment.center,
-            child: Container(
-              margin: const EdgeInsets.only(right: 4,top: 5),
-              height: 300,
-              width: 300,
-              decoration: BoxDecoration(
-                color: Colors.black, // Color does not matter but should not be transparent
-                borderRadius: BorderRadius.circular(300),
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                ),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 4,top: 5,bottom: 30),
+                    height: 300,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.black, // Color does not matter but should not be transparent
+                      borderRadius: BorderRadius.circular(300),
+                    ),
+                  ),
+                ),
               ),
-            ),
+
+            ],
           ),
         ),
       ],
